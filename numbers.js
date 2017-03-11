1fb1368f72ef23e2610c3ed61802ac6f80345c47
Danilo Alonso Sato <danilo.alonso@outlook.com>
2017-03-11 11:36:41 -0300

    Testes com números

---
+++ numbers.js
@@ -0,0 +1,11 @@
+var nota = 10;
+
+console.log(nota.toExponential(2));
+console.log(nota.toFixed(2));
+console.log(nota.toPrecision(1));
+console.log(nota.toString());
+console.log(nota.valueOf());
+
+console.log("-----------------");
+
+console.log(0.1 + 0.2);
