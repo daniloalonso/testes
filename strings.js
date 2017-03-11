88cf752a31b0c6e3de61f6b41eda2dda42be7527
Danilo Alonso Sato <danilo.alonso@outlook.com>
2017-03-11 12:24:39 -0300

    Modificações strings

--- strings.js
+++ strings.js
@@ -1,13 +1,19 @@
 console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
-var nome = "TesteJavaScript.1.2.3.4";
-
+console.log("0         1         2");
+console.log("01234567890123456789012456789");
+console.log("vvvvvvvvvvvvvvvvvvvvvvvvvvvvv");
+var nome =  "Teste.JavaScript.1.2.3.4";
 console.log(nome);
 console.log("");
-console.log("length       : " + nome.length);
-console.log("charAt 2     : " + nome.charAt(2));
-console.log("charCodeAt 0 : " + nome.charCodeAt(0));
-console.log("concat '!'   : " + nome.concat("!"));
-console.log("indexOf 'e'  : " + nome.indexOf('e'));
-console.log("replace      : " + nome.replace("TesteJava", "Teste Java"));
-console.log("split '.'    : " + nome.split("."));
+console.log("length         : " + nome.length);
+console.log("charAt 2       : " + nome.charAt(2));
+console.log("charCodeAt 0   : " + nome.charCodeAt(0));
+console.log("concat '!'     : " + nome.concat("!"));
+console.log("indexOf 'e'    : " + nome.indexOf('e'));
+console.log("replace        : " + nome.replace("Teste.Java", "Teste Java"));
+console.log("split '.'      : " + nome.split("."));
+console.log("match 'Java'   : " + nome.match("Java"));
+console.log("match 'java'   : " + nome.match("java"));
+console.log("search 'Java'  : " + nome.search("Java"));
+console.log("substring '17' : " + nome.substring(17));
