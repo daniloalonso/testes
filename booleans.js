6cc5d7218c873213e4bcdce333b017429413acd4
Danilo Alonso Sato <danilo.alonso@outlook.com>
2017-03-11 20:30:48 -0300

    Boolean adicionado!

---
+++ booleans.js
@@ -0,0 +1,13 @@
+console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+var teste1 = false;
+var teste2 = true;
+console.log(teste1.toString() + " " + teste2.valueOf());
+console.log("-------------------------");
+console.log(!!0);
+console.log(!!NaN);
+console.log(!!"");
+console.log(!!false);
+console.log(!!null);
+console.log(!!undefined);
+console.log("-------------------------");
+
