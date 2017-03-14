a3143520aeebea43efd35483bf793298b9cee9fc
Danilo Alonso Sato <danilo.alonso@outlook.com>
2017-03-13 23:50:02 -0300

    Date!

---
+++ datas.js
@@ -0,0 +1,45 @@
+/*var hoje = new Date();
+console.log(hoje.getTime());
+
+var natal = new Date(1419465600000);
+console.log(natal);
+console.log("RFC-2822 ------------------------");
+
+// RFC-2822
+var data1 = new Date("2014/12/25");
+var data2 = new Date("12/25/2014");
+var data3 = new Date("25/12/2014");
+var data4 = new Date("Thu Dec 25 2014");
+var data5 = new Date("Thu Dec 25 2014 00:00:12");
+console.log(data1);
+console.log(data2);
+console.log(data3);
+console.log(data4);
+console.log(data5);
+console.log("ISO 8601 ------------------------");
+
+// ISO 8601
+var data1 = new Date("2014-12-25");
+var data2 = new Date("2014-12-25T00:00:12");
+var data3 = new Date("2014-12-25T00:00:12Z");
+var data4 = new Date("2014-12-25T00:00:12-02:00");
+console.log(data1);
+console.log(data2);
+console.log(data3);
+console.log(data4);
+console.log("------------------------");
+var data1 = new Date(2014, 11, 25);
+var data2 = new Date(2014, 11, 25, 10, 30, 00);
+console.log(data1);
+console.log(data2);
+console.log("------------------------");
+*/
+var natal = new Date(2014, 11, 25, 8, 10, 0);
+console.log(natal.getYear());
+console.log(natal.getFullYear());
+console.log(natal.getMonth());
+console.log(natal.getDate());
+console.log(natal.getDay());
+console.log(natal.getMilliseconds());
+
+
